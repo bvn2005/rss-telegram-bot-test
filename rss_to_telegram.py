7b8d620fa7b7be722e1a104db9a687936af6cb07
bvn2005 <bvn2005@hotmail.com>
2026-06-15 16:29:49 +0300

    Update rss_to_telegram.py

--- rss_to_telegram.py
+++ rss_to_telegram.py
@@ -192,28 +192,28 @@ response.raise_for_status()
 
 MAX_LEN = 3500
 for i in range(0, len(article_text), MAX_LEN):
-    chunk = article_text[i:i + MAX_LEN]
-    chunk = html.escape(chunk)
-    requests.post(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-        json={
-            "chat_id": CHAT_ID,
-            "text": f"<blockquote>{chunk}</blockquote>",
-            "parse_mode": "HTML"
-        },  
-        timeout=30
-    )
-    
-    # article_text = html.escape(article_text)
+    # chunk = article_text[i:i + MAX_LEN]
+    # chunk = html.escape(chunk)
     # requests.post(
     #     f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
     #     json={
     #         "chat_id": CHAT_ID,
-    #         "text": f"<blockquote expandable>{article_text}</blockquote>",
+    #         "text": f"<blockquote>{chunk}</blockquote>",
     #         "parse_mode": "HTML"
-    #     },
+    #     },  
     #     timeout=30
     # )
+    
+    article_text = html.escape(article_text)
+    requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        json={
+            "chat_id": CHAT_ID,
+            "text": f"<blockquote expandable>{article_text}</blockquote>",
+            "parse_mode": "HTML"
+        },
+        timeout=30
+    )
 
 # =========================
 # Оновлення state.json
